2d2a63b0faec9195b4a6a2ae30d158612b2483a2
dracz <davidracz@gmail.com>
2015-06-03 12:46:50 -0700

    made prompt default arg

--- prompt.py
+++ prompt.py
@@ -6,7 +6,7 @@ def openurl(url):
     prompt("Press enter to open: '{}'...\n".format(url))
     webbrowser.open(url)
 
-def prompt(s):
+def prompt(s="Press any key to continue..."):
     try:
         return raw_input(s) # python 2
     except NameError:
