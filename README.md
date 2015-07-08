d7dc4fd2e1bfa3afa6365c4f4642d9da7c0dc755
dracz <davidracz@gmail.com>
2015-07-07 23:48:56 -0700

    updated readme and solver

--- README.md
+++ README.md
@@ -14,11 +14,26 @@ Level 11 and many others use the python imaging library (PIL) module: pillow:
 
     $ pip-3.4 install pillow
     
-Level 30 uses matplotlib to visualize the data
+Level 30 uses matplotlib to visualize the data.
 
-## Running
+*NB: I have only tested this on OS X Yosemite.*
 
-You can run each level with, for example:
 
-    $ python3.4 level0.py
+## Solving Levels
+
+You can run the solution for all levels with
+
+    $ python3.4 solve.py
+    
+Or solve a single level, with:
+
+    $ python3.4 solve.py 0
+    
+Or solve range or levels with:
+
+    $ python3.4 solve.py 0 10
+    
+Running the solution will prompt to open the next level page in the browser. When username/password are required,
+they are printed to the console before prompting to open. Various output from the solutions are typically printed 
+and any images that are generated are typically opened for preview.
 
--- solve.py
+++ solve.py
@@ -4,16 +4,40 @@ import sys, os
 
 LAST_LEVEL = 33
 
-if len(sys.argv) == 1:
-    start = 0
-    end = LAST_LEVEL + 1
+def print_usage():
+    print("""
+NAME
+     solve.py
 
-elif len(sys.argv) == 2:
-    start, end = int(sys.argv[1]), int(sys.argv[1])+1
+DESCRIPTION
+     solve one or more stages of http://www.pythonchallenge.com/
 
-elif len(sys.argv) > 2:
-    start, end = int(sys.argv[1]), int(sys.argv[2])+1
+USAGE
+     python3.4 solve.py [from_level [to_level]]
 
-for i in range(start, end):
-    print("solving level", i, "...")
-    os.system("python3.4 level{}.py".format(i))
+ARGS
+     if no levels, are specified then all are solved in sequence.
+     if just from_level is specified, then only that level is solved.
+     if to_level is also specified, then levels within the range are solved
+""")
+
+def main():
+    if len(sys.argv) == 1:
+        start = 0
+        end = LAST_LEVEL + 1
+
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == "?" or sys.argv[1] == "help":
+            return print_usage()
+
+        start, end = int(sys.argv[1]), int(sys.argv[1])+1
+
+    elif len(sys.argv) > 2:
+        start, end = int(sys.argv[1]), int(sys.argv[2])+1
+
+    for i in range(start, end):
+        print("solving level", i, "...")
+        os.system("python3.4 level{}.py".format(i))
+
+if __name__ == "__main__":
+    main()
